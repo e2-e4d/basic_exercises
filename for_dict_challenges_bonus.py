1d42984c612762a61dca47a24be7e7bcdd0ffd5b
Yura <akhmetshin.yura@gmail.com>
2026-03-21 02:07:10 +0300

    bonus

--- for_dict_challenges_bonus.py
+++ for_dict_challenges_bonus.py
@@ -38,7 +38,8 @@ import lorem
 
 
 def generate_chat_history():
-    messages_amount = random.randint(200, 1000)
+    # messages_amount = random.randint(200, 1000)
+    messages_amount = random.randint(3, 7)
     users_ids = list(
         {random.randint(1, 10000) for _ in range(random.randint(5, 20))}
     )
@@ -66,5 +67,54 @@ def generate_chat_history():
     return messages
 
 
+# 1. Вывести айди пользователя, который написал больше всех сообщений.
+def most_active_user_id(messages):
+    user_counts = {}
+    for message in messages:        
+        user_id = message['sent_by']
+        if user_id not in user_counts:
+            user_counts[user_id] = 1
+        else:
+            user_counts[user_id] += 1
+
+    print(user_counts)
+    return max(user_counts, key=user_counts.get)
+
+# 2. Вывести айди пользователя, на сообщения которого больше всего отвечали.
+def most_replied_user_id(messages):
+    reply_counts = {}
+    for message in messages:
+        message_reply_for = message['reply_for']
+        if message_reply_for not in reply_counts:
+            reply_counts[message_reply_for] = 1
+        else:
+            reply_counts[message_reply_for] += 1
+
+    print(reply_counts)
+    return max(reply_counts, key=reply_counts.get)  
+
+# 3. Вывести айди пользователей, сообщения которых видело больше всего уникальных пользователей.
+def most_viewed_user_ids(messages):
+    pass    
+
+# 4. Определить, когда в чате больше всего сообщений: утром (до 12 часов), днём (12-18 часов) или вечером (после 18 часов).
+def peak_time_of_day(messages):
+    pass
+
+# 5. Вывести идентификаторы сообщений, который стали началом для самых длинных тредов (цепочек ответов).
+def longest_thread_starter_ids(messages):
+    pass
+
 if __name__ == "__main__":
     print(generate_chat_history())
+    chat_messages = generate_chat_history()
+    print('\n')
+    print("1. Идентификатор пользователя, который написал больше всех сообщений:", most_active_user_id(chat_messages))
+    print('\n')
+    print("2. Идентификатор пользователя, на сообщения которого больше всего отвечали:", most_replied_user_id(chat_messages))
+    print('\n')
+    print("3. Идентификатор пользователей, сообщения которых видело больше всего уникальных пользователей:", most_active_user_id(chat_messages))
+    print('\n')
+    print("4. В чате больше всего сообщений:", most_active_user_id(chat_messages))
+    print('\n')
+    print("5. Идентификатор сообщения, которое стало началом для самого длинного треда", most_active_user_id(chat_messages))
